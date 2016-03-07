c407f791db3d67be3b6bcc2191172383128fcd58
shigemk2 <i.am.shige@gmail.com>
2016-03-07 19:55:47 +0900

    Compare now and cron time

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,10 +1,14 @@
 package com.example
 import cronish._
-import dsl._
+import scalendar.Scalendar
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Every day at midnight".crons == "0 0 * * *")
-    println("Hello, world!")
+    // Cron(second, minute, hour, day, month, year)
+    val cron = Cron("0", "0", "*", "*", "*", "*", "*") // every hour
+    val now = Scalendar.now
+    println((cron.nextFrom(now))) // returns milliseconds to next cron
+    println((cron.nextFrom(now) / 1000) % 60) // returns seconds to next cron
+    println((cron.nextFrom(now) / (1000*60) % 60))
   }
 }
