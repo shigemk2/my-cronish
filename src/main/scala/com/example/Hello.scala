a0fccaf4b7a81d2f0f14f1746ddfe225108e936d
shigemk2 <i.am.shige@gmail.com>
2016-03-07 19:35:20 +0900

    Add cronish

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,10 @@
 package com.example
+import cronish._
+import dsl._
 
 object Hello {
   def main(args: Array[String]): Unit = {
+    println("Every day at midnight".crons == "0 0 * * *")
     println("Hello, world!")
   }
 }
